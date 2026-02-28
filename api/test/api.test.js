f9a8497d1ef7b23a8c350c32eb0301067be2f824
Lynn Pyae Hmue <orionlynn@Lynns-MacBook-Air-5.local>
2026-02-28 23:43:22 +0700

    devops setup 1.1

--- api/test/api.test.js
+++ api/test/api.test.js
@@ -1,6 +1,16 @@
 const request = require('supertest');
 const app = require('../src/index');
 
+let server;
+
+beforeAll(() => {
+  server = app.listen(0); 
+});
+
+afterAll((done) => {
+  server.close(done); 
+});
+
 describe('GET /health', () => {
   it('should return 200 with status ok', async () => {
     const res = await request(app).get('/health');
